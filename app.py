86663182b644abf9b06674369cd2a325eb0243e8
faizabatta1 <faizabatta1@github.com>
2023-06-25 06:51:35 +0300

    final server update

--- app.py
+++ app.py
@@ -2,7 +2,17 @@ from flask import Flask, request, jsonify
 import os
 from script import translate
 
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app, origins='*', methods=['GET', 'POST', 'PUT', 'DELETE'], allow_headers='*')
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    return response
 
 @app.post('/translate-image')
 def translate_image():
@@ -25,4 +35,4 @@ def translate_image():
     return translation
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=5020,debug=True)
